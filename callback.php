e2a7b531d4ea9825d90d4af71850cc914209955d
Sakura Mao <foole852741@gmail.com>
2017-06-08 23:07:28 +0900

    updata on new project

---
+++ callback.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Copyright 2016 LINE Corporation
+ *
+ * LINE Corporation licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+require_once('./LINEBotTiny.php');
+
+$channelAccessToken = '8nhSzW2bEnjdL3vqCmzBnLwyXc8Di5NDN+RmKrmnQM5/9vI44kJsGdaceLC44PE9oCz7F5MjE1u7l9ypC7L6TjFwJ6lVXdtDx7ms6nWlG+7YUvJ0/cVxbDruLw6RSklmEdXSIdNk6LPdDu09Z1mZtwdB04t89/1O/w1cDnyilFU=';
+$channelSecret = 'ed9a2a898ff89148e1ef403f63703c31';
+
+$client = new LINEBotTiny($channelAccessToken, $channelSecret);
+foreach ($client->parseEvents() as $event) {
+    switch ($event['type']) {
+        case 'message':
+            $message = $event['message'];
+            switch ($message['type']) {
+                case 'text':
+                    $client->replyMessage(array(
+                        'replyToken' => $event['replyToken'],
+                        'messages' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => $message['text']
+                            )
+                        )
+                    ));
+                    break;
+                default:
+                    error_log("Unsupporeted message type: " . $message['type']);
+                    break;
+            }
+            break;
+        default:
+            error_log("Unsupporeted event type: " . $event['type']);
+            break;
+    }
+};
